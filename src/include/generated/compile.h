8266f442e363718d6661781bd2c0f9c764b0790b
忻 <140865993+Imlossss@users.noreply.github.com>
2024-06-06 21:40:01 +0800

    Delete src directory

--- src/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 9 */
-/* SMP */
-#define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#9 SMP Tue Mar 5 19:51:57 CST 2024"
-#define LINUX_COMPILE_BY "gni"
-#define LINUX_COMPILE_HOST "gni-none"
-#define LINUX_COMPILER "gcc version 7.5.0 (Ubuntu 7.5.0-3ubuntu1~18.04)"
